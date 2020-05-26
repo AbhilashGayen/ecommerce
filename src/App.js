bd1380afe13debf98af404b7a4c9bdd4a9cb0ff2
Abhilash Gayen <abhilashgayen@Abhilashs-MacBook-Pro.local>
2020-05-26 23:08:01 +0530

    Added header, shop page and collection preview

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
-import HomePage from './pages/HomePage/HomePage.component';
+import HomePage from './pages/home/home-page.component';
 
 import './App.css';
+import ShopPage from './pages/shop/shop-page.component';
+import Header from './components/header/header.component';
 
 class App extends React.Component {
   render() {
     return (
-      <div className='App'>
-        <HomePage />
+      <div>
+        
+        <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+        </Switch>
+        </BrowserRouter>
       </div>
     );
   }
